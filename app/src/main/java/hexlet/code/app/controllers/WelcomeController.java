f0779181e4ffbacff3894a27c542f9db9516229e
andreevmo <maxiandreev1994@gmail.com>
2022-12-16 18:02:05 +0400

    feat: add linter

--- app/src/main/java/hexlet/code/app/controllers/WelcomeController.java
+++ app/src/main/java/hexlet/code/app/controllers/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public final class WelcomeController {
 
     @GetMapping(path = "/welcome")
     public String greet() {
