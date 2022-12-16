ad0346bbdf98140c90144ff7b98ff1523b7e8dca
andreevmo <maxiandreev1994@gmail.com>
2022-12-16 18:49:48 +0400

    feat: add plugin Jacoco and test

--- app/src/test/java/hexlet/code/app/AppApplicationTests.java
+++ app/src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,8 +1,29 @@
 package hexlet.code.app;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void test() throws Exception {
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/welcome"))
+                .andReturn()
+                .getResponse();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).contains("Welcome to Spring");
+    }
 }
