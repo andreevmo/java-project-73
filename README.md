8015f1c62d4aa0b10666c8ed2aa664c26f7ffef9
andreevmo <maxiandreev1994@gmail.com>
2022-12-16 17:15:24 +0400

    feat: prepare project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/andreevmo/java-project-73/workflows/hexlet-check/badge.svg)](https://github.com/andreevmo/java-project-73/actions)
+
+### Link to App:
+https://my-spring-web.onrender.com
---
+++ app/src/main/java/hexlet/code/app/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package hexlet.code.app.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(path = "/welcome")
+    public String greet() {
+        return "Welcome to Spring";
+    }
+}
