e76c4447694ca5f7f7af543e35adf872527b64b7
andreevmo <maxiandreev1994@gmail.com>
2023-01-12 22:30:18 +0400

    feat: setup database and create entity

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,39 @@
+package hexlet.code.app.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+}
---
+++ app/src/main/java/hexlet/code/app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package hexlet.code.app.repository;
+
+import hexlet.code.app.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
