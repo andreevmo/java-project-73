fdbf94511a5d42540989e21be533692f188a8fde
andreevmo <maxiandreev1994@gmail.com>
2023-02-15 17:19:39 +0400

    feat: update CustomExceptionHandler

--- src/main/java/hexlet/code/app/exception/CustomExceptionHandler.java
+++ src/main/java/hexlet/code/app/exception/CustomExceptionHandler.java
@@ -1,43 +1,42 @@
 package hexlet.code.app.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
 
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(org.hibernate.exception.ConstraintViolationException.class)
-    public ResponseEntity<String> incorrectInputException(org.hibernate.exception.ConstraintViolationException e) {
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body("Incorrect input");
+    public String incorrectInputException(ConstraintViolationException e) {
+        return e.getMessage();
     }
 
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> incorrectInputException(MethodArgumentNotValidException e) {
-        return ResponseEntity
-                .status(HttpStatus.UNPROCESSABLE_ENTITY)
-                .body("Incorrect input");
+    public List<ObjectError> incorrectInputException(MethodArgumentNotValidException e) {
+        return e.getAllErrors();
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> objectNotFound(NoSuchElementException e) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body("Object not found");
+    public String objectNotFound(NoSuchElementException e) {
+        return e.getMessage();
     }
 
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<String> authException(AuthenticationException e) {
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body("Access denied");
+    public String authException(AuthenticationException e) {
+        return e.getMessage();
     }
 }
