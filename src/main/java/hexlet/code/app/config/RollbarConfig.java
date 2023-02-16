7e18f2c08f28b209ca25f9c65fff5b3a43253f8c
andreevmo <maxiandreev1994@gmail.com>
2023-02-16 13:49:00 +0400

    feat: setup Rollbar

---
+++ src/main/java/hexlet/code/app/config/RollbarConfig.java
@@ -0,0 +1,33 @@
+package hexlet.code.app.config;
+
+import com.rollbar.notifier.Rollbar;
+import com.rollbar.notifier.config.Config;
+import com.rollbar.spring.webmvc.RollbarSpringConfigBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration()
+@ComponentScan({
+    "hexlet.code.app",
+    "com.rollbar.spring"
+})
+public class RollbarConfig {
+
+    /**
+     * Register a Rollbar bean to configure App with Rollbar.
+     */
+
+    public static final String ROLLBAR_TOKEN = System.getenv("ROLLBAR_TOKEN");
+    public static final String APP_ENV = System.getenv("APP_ENV");
+    @Bean
+    public Rollbar rollbar() {
+        return new Rollbar(getRollbarConfigs());
+    }
+
+    private Config getRollbarConfigs() {
+        return RollbarSpringConfigBuilder.withAccessToken(RollbarConfig.ROLLBAR_TOKEN)
+                .environment(APP_ENV)
+                .build();
+    }
+}
