30ff03d0ff33767bee59ef57745cd0bbcc8ef4eb
andreevmo <maxiandreev1994@gmail.com>
2023-02-19 00:10:09 +0400

    feat: update CustomExceptionHandler

--- src/main/java/hexlet/code/exception/CustomExceptionHandler.java
+++ src/main/java/hexlet/code/exception/CustomExceptionHandler.java
@@ -1,44 +1,71 @@
 package hexlet.code.exception;
 
 import io.swagger.v3.oas.annotations.Hidden;
-import org.hibernate.exception.ConstraintViolationException;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+
 @Hidden
-@RestControllerAdvice
+@ResponseBody
+@ControllerAdvice
 public class CustomExceptionHandler {
 
-
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(org.hibernate.exception.ConstraintViolationException.class)
-    public String incorrectInputException(ConstraintViolationException e) {
-        return e.getMessage();
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public String generalExceptionHandler(Exception exception) {
+        return exception.getMessage();
     }
 
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ObjectError> incorrectInputException(MethodArgumentNotValidException e) {
-        return e.getAllErrors();
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NoSuchElementException.class)
-    public String objectNotFound(NoSuchElementException e) {
-        return e.getMessage();
+    public String noSuchElementExceptionHandler(NoSuchElementException exception) {
+        return exception.getMessage();
     }
 
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    @ExceptionHandler(AuthenticationException.class)
-    public String authException(AuthenticationException e) {
-        return e.getMessage();
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public String validationExceptionsHandler(Exception exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ObjectError> validationExceptionsHandler(MethodArgumentNotValidException exception) {
+        return exception.getAllErrors();
+    }
+
+    @ResponseStatus(UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public String validationExceptionsHandler(DataIntegrityViolationException exception) {
+        return exception.getCause().getCause().getMessage();
+    }
+
+    @ResponseStatus(FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDeniedException(AccessDeniedException exception) {
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(UNAUTHORIZED)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public String userNitFoundExceptionHandler(UsernameNotFoundException exception) {
+        return exception.getMessage();
     }
 }
